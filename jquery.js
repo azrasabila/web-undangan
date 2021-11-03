6598a824c513be44c67e93a781451fedc6f15679
Azra <azra@agate.id>
2021-11-03 20:13:26 +0700

    fix video size, fix stretch

--- jquery.js
+++ jquery.js
@@ -79,13 +79,13 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("waktu").innerHTML = days + "<br> Hari ";
+  document.getElementById("waktu").innerHTML = days + "<br> Hari";
 
-  document.getElementById("jam").innerHTML = hours + "<br> jam ";
+  document.getElementById("jam").innerHTML = hours + "<br> jam";
 
-  document.getElementById("menit").innerHTML = minutes + "<br> menit ";
+  document.getElementById("menit").innerHTML = minutes + "<br> menit";
 
-  document.getElementById("detik").innerHTML = seconds + "<br> detik ";
+  document.getElementById("detik").innerHTML = seconds + "<br> detik";
 
   // If the count down is finished, write some text
   if (distance < 0) {

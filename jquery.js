e1825ddf9c511b775a3d6d3a4737e87bb546610c
Azra <azra@agate.id>
2021-11-20 18:03:20 +0700

    add announcement page

--- jquery.js
+++ jquery.js
@@ -95,7 +95,7 @@ async function loadData() {
 loadData()
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 5, 2021 10:00:00").getTime();
+var countDownDate = new Date("Dec 5, 2021 8:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
